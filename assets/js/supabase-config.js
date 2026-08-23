09c52a8f067f9d25a7111ed792814f0cd4b04b13
speakoutMentalHealth <speaaakout@gmail.com>
2026-08-23 14:54:22 +0100

    Update supabase-config.js

--- assets/js/supabase-config.js
+++ assets/js/supabase-config.js
@@ -1,5 +1,5 @@
 window.KPA_SUPABASE_CONFIG = {
-  url: "db.foewiwvrjshdxtljxbtq.supabase.co",
+  url: "https://db.foewiwvrjshdxtljxbtq.supabase.co",
   anonKey: "sb_publishable_JW6eR7W4J0o72sjRj3AZlA_Sv8COIJt",
   speakout: {
     signupUrl: "https://speakoutmentalhealth.org/auth.html#register",
