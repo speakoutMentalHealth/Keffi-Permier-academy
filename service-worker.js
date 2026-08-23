feacdc69305d06c2116d803a2a243d7b1d10744b
speakoutMentalHealth <speaaakout@gmail.com>
2026-08-23 16:48:16 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='kpa-v14-production';
+const CACHE='kpa-v15-compass';
 const STATIC=[
  './assets/css/styles.css','./assets/js/main.js','./assets/images/kpa-logo.jpg',
  './assets/svg/living-crest.svg','./assets/svg/kpa-geometry.svg'
